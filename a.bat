ae394d5b7da7204fbedfdc2add56b0343e93437e
phioa <phioa@phioa.xyz>
2021-07-14 17:06:29 +0800

    test

---
+++ a.bat
@@ -0,0 +1,10 @@
+@echo off
+echo 本次commit的注释：
+set/p "commit="
+echo push到远端仓库的哪个分支?：
+set/p "branch="
+if "%branch%" == "" (set "branch=master")
+git add .
+git commit -m "%commit%"
+git push origin %branch%
+pause
