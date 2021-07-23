d6d6cf66ede5856ef333959261840b80f2162bae
phioa <phioa@phioa.xyz>
2021-07-23 13:08:12 +0800

    点赞功能初步调试完成，已经可以使用

--- source/js/phioa_likes/phioa_likes.js
+++ source/js/phioa_likes/phioa_likes.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var getLikes_ajaxing = false;
+var like_ajaxing = false;
 
 function getLikes(){
 	if(!getLikes_ajaxing){
@@ -9,6 +10,7 @@ function getLikes(){
 			"https://www.phioa.xyz:8443/url_likes/getLikes.php", 
 			{url: window.location.href},
 			function(result){
+				console.log(result);
 				if(result.success){
 					jQuery('#phioa_likes-count').text(function(){
 						return result.data.likedTimes;
@@ -27,10 +29,34 @@ function getLikes(){
 					
 				}
 				getLikes_ajaxing = false;
-			}
+			}	
 		);
 	}
 }
 
+function like(){
+	if(jQuery('#phioa_likes-icon').hasClass('beforelike')){
+		jQuery('#phioa_likes-icon').removeClass('beforelike');
+		jQuery('#phioa_likes-icon').addClass('afterlike');
+		jQuery('#phioa_likes-count').text(function(){return parseInt(jQuery('#phioa_likes-count').text())+1;});
+	} else {
+		jQuery('#phioa_likes-icon').removeClass('afterlike');
+		jQuery('#phioa_likes-icon').addClass('beforelike');
+		jQuery('#phioa_likes-count').text(function(){return parseInt(jQuery('#phioa_likes-count').text())-1;});
+	}
+	if(!like_ajaxing){
+		like_ajaxing = true;
+		jQuery.getJSON(
+			"https://www.phioa.xyz:8443/url_likes/like.php", 
+			{url: window.location.href},
+			function(result){
+				console.log(result);
+				like_ajaxing = false;
+			}	
+		);
+	}
+}
+
+
 jQuery(getLikes);
-jQuery('#phioa_likes').click();
+jQuery('#phioa_likes').click(like);
