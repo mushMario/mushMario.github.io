6fe7d289729b9a63e369d5fff439c5380df1c609
phioa <phioa@phioa.xyz>
2021-07-23 13:15:46 +0800

    添加了不允许用户在点赞信息获取完成前点赞的功能

--- source/js/phioa_likes/phioa_likes.js
+++ source/js/phioa_likes/phioa_likes.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var likeReady = false;
+
 function getLikes(){
 	jQuery.getJSON(
 		"https://www.phioa.xyz:8443/url_likes/getLikes.php", 
@@ -21,30 +23,31 @@ function getLikes(){
 					}
 					jQuery('#phioa_likes-icon').addClass('beforelike');
 				}
-				
+				likeReady = true;
 			}
 		}	
 	);
 }
 
 function like(){
-	if(jQuery('#phioa_likes-icon').hasClass('beforelike')){
-		jQuery('#phioa_likes-icon').removeClass('beforelike');
-		jQuery('#phioa_likes-icon').addClass('afterlike');
-		jQuery('#phioa_likes-count').text(function(){return parseInt(jQuery('#phioa_likes-count').text())+1;});
-	} else {
-		jQuery('#phioa_likes-icon').removeClass('afterlike');
-		jQuery('#phioa_likes-icon').addClass('beforelike');
-		jQuery('#phioa_likes-count').text(function(){return parseInt(jQuery('#phioa_likes-count').text())-1;});
+	if(likeReady){
+		if(jQuery('#phioa_likes-icon').hasClass('beforelike')){
+			jQuery('#phioa_likes-icon').removeClass('beforelike');
+			jQuery('#phioa_likes-icon').addClass('afterlike');
+			jQuery('#phioa_likes-count').text(function(){return parseInt(jQuery('#phioa_likes-count').text())+1;});
+		} else {
+			jQuery('#phioa_likes-icon').removeClass('afterlike');
+			jQuery('#phioa_likes-icon').addClass('beforelike');
+			jQuery('#phioa_likes-count').text(function(){return parseInt(jQuery('#phioa_likes-count').text())-1;});
+		}
+		jQuery.getJSON(
+			"https://www.phioa.xyz:8443/url_likes/like.php", 
+			{url: window.location.href},
+			function(result){
+				console.log(result);
+			}	
+		);
 	}
-	jQuery.getJSON(
-		"https://www.phioa.xyz:8443/url_likes/like.php", 
-		{url: window.location.href},
-		function(result){
-			console.log(result);
-			like_ajaxing = false;
-		}	
-	);
 }
 
 
