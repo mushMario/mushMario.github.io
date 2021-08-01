328e19e9bd275f84d5819d0b1764a2bcce58f2cc
phioa <phioa@phioa.xyz>
2021-08-01 15:55:07 +0800

    写了一点注释

--- source/js/phioa_likes/phioa_likes.js
+++ source/js/phioa_likes/phioa_likes.js
@@ -1,21 +1,26 @@
 (function(){
 	'use strict';
 
-	if(jQuery('#phioa_likes').length <= 0)
+	if(jQuery('#phioa_likes').length <= 0) // 检查当前页面是否有#phioa_likes标签
 		return;
 	
-	let likeReady = false;
-
+	let likeReady = false; // 这个变量用于标记当前页面点赞数获取是否已经完成
+	
+	// 在页面dom加载完成时会试图获取当前页面点赞数
 	jQuery(function(){
 		jQuery.getJSON(
-			"https://www.phioa.xyz:8443/url_likes/getLikes.php", 
-			{url: window.location.href},
+			"https://www.phioa.xyz:8443/url_likes/getLikes.php", // 后端部分做成了开放api接口的形式
+			{url: window.location.href}, // 我有好好做sql注入防护，所以别乱想噢！
 			function(result){
 				console.log(result);
-				if(result.success){
+				/* 如果有报错的话，也可以在mush的mushmario.github.io仓库提交issue噢）
+				 * https://github.com/mushMario/mushMario.github.io/issues
+				 */
+				if(result.success){ // 返回json的简单处理
 					jQuery('#phioa_likes-count').text(function(){
 						return result.data.likedTimes;
 					});
+					// 非常笨笨的css操作
 					if(result.data.hasLiked){
 						if(jQuery('#phioa_likes-icon').hasClass('beforelike')){
 							jQuery('#phioa_likes-icon').removeClass('beforelike');
@@ -27,15 +32,18 @@
 						}
 						jQuery('#phioa_likes-icon').addClass('beforelike');
 					}
+					
 					likeReady = true;
 					jQuery('#phioa_likes').css('display', 'inline');
 				}
 			}	
 		);
 	});
-
+	
+	// 用户点赞操作部分
 	jQuery('#phioa_likes').click(function(){
-		if(likeReady){
+		if(likeReady){ // 禁止用户在点赞数获取完成前就试图点赞（虽然貌似更改display属性也可以实现这一点
+			// 依旧还是非常笨笨的css操作
 			if(jQuery('#phioa_likes-icon').hasClass('beforelike')){
 				jQuery('#phioa_likes-icon').removeClass('beforelike');
 				jQuery('#phioa_likes-icon').addClass('afterlike');
@@ -45,11 +53,15 @@
 				jQuery('#phioa_likes-icon').addClass('beforelike');
 				jQuery('#phioa_likes-count').text(function(){return parseInt(jQuery('#phioa_likes-count').text())-1;});
 			}
+			// ajax部分
 			jQuery.getJSON(
 				"https://www.phioa.xyz:8443/url_likes/like.php", 
 				{url: window.location.href},
 				function(result){
-					console.log(result);
+					console.log(result); 
+					/* 如果有报错的话，也可以在mush的mushmario.github.io仓库提交issue噢）
+					 * https://github.com/mushMario/mushMario.github.io/issues
+					 */
 				}	
 			);
 		}
